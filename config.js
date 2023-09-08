dc07e94180107c309784aad84dace793d621eb45
Freshpresh05 <142161882+Freshpresh05@users.noreply.github.com>
2023-09-07 20:51:49 -0400

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['08138902126', 'GURU', true],
+  ['08138902126', 'og', true], 
   [''] 
 ] //Number of owners
 
@@ -48,7 +48,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'preshbot'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
